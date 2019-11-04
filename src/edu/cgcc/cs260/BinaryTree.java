e8717262fe99e7537fe0584df94df3ef8ca266e5
aburke-cgcc <55819868+aburke-cgcc@users.noreply.github.com>
2019-11-04 14:18:22 -0800

    Work on BST tree and Node Switchover to person.

--- src/edu/cgcc/cs260/BinaryTree.java
+++ src/edu/cgcc/cs260/BinaryTree.java
@@ -3,18 +3,19 @@ package edu.cgcc.cs260;
 public class BinaryTree {
 	
 	//members
-	Node root;
+	private BSTNode root;
 	
 	//constructor
-	BinaryTree(Node rt){
+	BinaryTree(BSTNode rt){
 		root = rt;
 	}
 	
 	//private member functions
+	private void printData(BSTNode rt) {
+		System.out.print("(" + rt.getKey() + ")" + rt.getData().getName() + " ");
+	}
 	
-	
-	//public member functions
-	public void traversePreOrder(Node rt) {
+	private void printPreOrder(BSTNode rt) {
 		
 		//base case: if node is null, return
 		if(rt == null) {
@@ -22,12 +23,12 @@ public class BinaryTree {
 		}
 		
 		//print pre order
-		System.out.print(rt.data + " ");
-		traversePreOrder(rt.left);
-		traversePreOrder(rt.right);
+		printData(rt);
+		printPreOrder(rt.left);
+		printPreOrder(rt.right);
 	}
 	
-	public void traverseInOrder(Node rt) {
+	private void printInOrder(BSTNode rt) {
 		
 		//base case: if node is null, return
 		if(rt == null) {
@@ -35,12 +36,12 @@ public class BinaryTree {
 		}
 		
 		//print in order
-		traverseInOrder(rt.left);
-		System.out.print(rt.data + " ");
-		traverseInOrder(rt.right);
+		printInOrder(rt.left);
+		printData(rt);
+		printInOrder(rt.right);
 	}
 	
-	public void traversePostOrder(Node rt) {
+	private void printPostOrder(BSTNode rt) {
 		
 		//base case: if node is null, return
 		if(rt == null) {
@@ -48,10 +49,108 @@ public class BinaryTree {
 		}
 		
 		//print post order
-		traversePostOrder(rt.left);
-		traversePostOrder(rt.right);
-		System.out.print(rt.data + " ");
+		printPostOrder(rt.left);
+		printPostOrder(rt.right);
+		printData(rt);
 	}
+	
+	private int getSize(BSTNode rt) {
+		int size = 0;
+		
+		//base case, size is zero if root is null
+		if(rt == null)
+			return size;
+		
+		//Get the size of tree
+		size += getSize(rt.left);
+		size += getSize(rt.right);
+		
+		return size;
+	}
+	
+	private int height(BSTNode rt) {
+		//base case: root is null
+		if(rt == null)
+			return 0;
+		else {
+			//get height of each sub tree
+			int lHeight = height(rt.left);
+			int rHeight = height(root.right);
+			
+			//return the largest
+			if(lHeight > rHeight)
+				return lHeight + 1;
+			else
+				return rHeight + 1;
+		}
+	}
+	
+	private void printLevel(BSTNode rt, int level) {
+		//base case
+		if(rt == null)
+			return;
+		
+		//Start at level 1 and continue on each branch
+		if(level == 1)
+			printData(rt);
+		else if(level > 1) {
+			printLevel(rt.left, level - 1);
+			printLevel(rt.right, level - 1);
+		}
+	}
+	
+	//insert data recursively
+	private BSTNode insert(BSTNode node, int key) {
+		//base case: if null, create new node
+		if(node == null)
+			node = new BSTNode(key);
+		//otherwise: compare keys and insert into correct location
+		else {
+			if(key <= node.getKey())
+				node.left = insert(node.left, key);
+			else
+				node.right = insert(node.right, key);
+		}
+		
+		return node;
+	}
+	
+	//public member functions
+	public void insert(int key) {
+		root = insert(root, key);
+	}
+	
+	public boolean isEmpty() {
+		return (root == null);
+	}
+	
+	public int nodeCount() {
+		return getSize(root);
+	}
+	
+	public void preOrder() {
+		printPreOrder(root);
+	}
+	
+	public void inOrder() {
+		printInOrder(root);
+	}
+	
+	public void postOrder() {
+		printPostOrder(root);
+	}
+	
+	public void levelOrder() {
+		
+		//get the height of the tree
+		int height = height(root);
+		
+		//print each height
+		for(int i = 1; i <= height; i++)
+			printLevel(root, i);
+	}
+	
+	
 	
 	
 }
--- src/edu/cgcc/cs260/Main.java
+++ src/edu/cgcc/cs260/Main.java
@@ -1,60 +1,57 @@
 package edu.cgcc.cs260;
-import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
 		//recurse through file system
-		System.out.println("\n\n\nRecursion Get:\n");
-		Recursion.q.clear();
-		Recursion.recursionGet(Recursion.root);
-		
-		while(!Recursion.q.isEmpty()) {
-			System.out.println(Recursion.q.remove());
-		}
+		/*
+		 * System.out.println("\n\n\nRecursion Get:\n"); Recursion.q.clear();
+		 * Recursion.recursionGet(Recursion.root);
+		 * 
+		 * while(!Recursion.q.isEmpty()) { System.out.println(Recursion.q.remove()); }
+		 */
 		
 		
 		//iterate through file system
-		System.out.println("\n\n\nIterative Get:\n");
-		Recursion.iterativeGet(Recursion.root);
+		/*
+		 * System.out.println("\n\n\nIterative Get:\n");
+		 * Recursion.iterativeGet(Recursion.root);
+		 * 
+		 * while(!Recursion.q.isEmpty()) { System.out.println(Recursion.q.remove()); }
+		 */
 		
-		while(!Recursion.q.isEmpty()) {
-			System.out.println(Recursion.q.remove());
-		}
 		
+		/*
+		 * //Create tree nodes BSTNode a = new BSTNode("A"); BSTNode b = new
+		 * BSTNode("B"); BSTNode c = new BSTNode("C"); BSTNode d = new BSTNode("D");
+		 * BSTNode e = new BSTNode("E"); BSTNode f = new BSTNode("F"); BSTNode g = new
+		 * BSTNode("G"); BSTNode h = new BSTNode("H"); BSTNode i = new BSTNode("I");
+		 * BSTNode j = new BSTNode("J"); BSTNode k = new BSTNode("K");
+		 * 
+		 * //build tree manually BSTNode root = a; root.left = b; root.right = c; b.left
+		 * = d; b.right = e; d.left = h; d.right = i; e.left = j; c.left = f; c.right =
+		 * g; g.left = k;
+		 * 
+		 * BinaryTree bt = new BinaryTree(root);
+		 * 
+		 * System.out.print("Pre Order Traverse: "); bt.preOrder();
+		 * System.out.println(); System.out.print("In Order Traverse: "); bt.inOrder();
+		 * System.out.println(); System.out.print("Post Order Traverse: ");
+		 * bt.postOrder();
+		 */
+		BinaryTree bt = new BinaryTree(null);
+		bt.insert(11);
+		bt.insert(6);
+		bt.insert(8);
+		bt.insert(19);
+		bt.insert(4);
+		bt.insert(10);
+		bt.insert(5);
+		bt.insert(17);
+		bt.insert(43);
+		bt.insert(49);
+		bt.insert(31);
 		
-		//Create tree nodes
-		Node a = new Node("A");
-		Node b = new Node("B");
-		Node c = new Node("C");
-		Node d = new Node("D");
-		Node e = new Node("E");
-		Node f = new Node("F");
-		Node g = new Node("G");
-		Node h = new Node("H");
-		Node i = new Node("I");
-		Node j = new Node("J");
-		Node k = new Node("K");
-		
-		//build tree manually
-		Node root = a;
-		root.left = b; root.right = c;
-		b.left = d; b.right = e;
-		d.left = h; d.right = i;
-		e.left = j;
-		c.left = f; c.right = g;
-		g.left = k;
-		
-		BinaryTree bt = new BinaryTree(root);
-		
-		System.out.print("In Order Traverse: ");
-		bt.traverseInOrder(bt.root);
-		System.out.println();
-		System.out.print("Pre Order Traverse: ");
-		bt.traversePreOrder(bt.root);
-		System.out.println();
-		System.out.print("Post Order Traverse: ");
-		bt.traversePostOrder(bt.root);
-		
+		bt.levelOrder();
 	}
 
 }
